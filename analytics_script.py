d1da53e59adf8524b00a3cf3ba78ab2ae3b44974
irfan <muhdirfansyafi@gmail.com>
2024-01-29 19:58:58 +0800

    Adding important files

---
+++ analytics_script.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import logging
+import os
+logging.getLogger().setLevel(logging.INFO)
+
+donation_facilities_url = "https://raw.githubusercontent.com/MoH-Malaysia/data-darah-public/main/donations_facility.csv"
+donation_state_url = "https://raw.githubusercontent.com/MoH-Malaysia/data-darah-public/main/donations_state.csv"
+newdonors_facility_url = "https://raw.githubusercontent.com/MoH-Malaysia/data-darah-public/main/newdonors_facility.csv"
+newdonors_state_url = "https://raw.githubusercontent.com/MoH-Malaysia/data-darah-public/main/newdonors_state.csv"
+
+# Get data from the url
+def get_data_url(url_link):
+    try:
+        data = pd.read_csv(url_link) # , parse_dates=['date'], index_col='date'
+        logging.info(f"Getting data from {url_link}")
+        return data
+    except Exception as e:
+        logging.error(f"Unexpected error fetching data from {url_link}: {e}")
+        return None
+
+donation_facilities = get_data_url(donation_facilities_url)
+donation_state = get_data_url(donation_state_url)
+newdonors_facility = get_data_url(newdonors_facility_url)
+newdonors_state = get_data_url(newdonors_state_url)
+
+blood_donation_retention = pd.read_parquet("https://dub.sh/ds-data-granular") # In this parquet, there is no date
+
+
+# Append data into new csv file
+def append_data(data, file_path):
+    if os.path.exists(file_path):
+        existing_data = pd.read_csv(file_path)
+        updated_data = pd.merge(existing_data, data, how='outer', indicator=True).query('_merge == "right_only"').drop('_merge', axis=1)
+    else:
+        updated_data = data
+        logging.info(f"File not exist. Create new file")
+
+    updated_data.to_csv(file_path, index=False, mode='a', header=not os.path.exists(file_path))
+    logging.info(f"Data appended to {file_path}.")
+
+    return pd.read_csv(file_path)
+
+donation_facilities_updated = append_data(donation_facilities, "donation_facilities.csv")
+donation_state_updated = append_data(donation_state, "donation_state.csv")
+newdonors_facility_updated = append_data(newdonors_facility,"newdonors_facility.csv")
+newdonors_state_updated = append_data(newdonors_state, "newdonors_state.csv")
+blood_donation_retention_updated = append_data(blood_donation_retention, "blood_donation_retention.csv")
+
+# Change date time to datetimeindex
+def change_dataframe_timeindex(dataframe):
+    dataframe['date'] = pd.to_datetime(dataframe['date'],errors='coerce')
+    dataframe = dataframe.set_index('date', inplace=True)
+
+
+change_dataframe_timeindex(donation_facilities_updated)
+change_dataframe_timeindex(donation_state_updated)
+change_dataframe_timeindex(newdonors_facility_updated)
+change_dataframe_timeindex(newdonors_state_updated)
+
+
+# Transform data and visualization for yearly total donation on each blood type
+
+import matplotlib.pyplot as plt
+oldest_date = donation_state_updated.index.min()
+newest_date = donation_state_updated.index.max()
+print(oldest_date)
+print(newest_date)
+
+donation_state_updated_resampled = donation_state_updated.resample('Y').sum()
+donation_state_updated_resampled = donation_state_updated_resampled.loc[oldest_date:newest_date]
+
+donation_state_updated_resampled['blood_a'].plot(label='Blood Type A', color='red')
+donation_state_updated_resampled['blood_ab'].plot(label='Blood Type AB', color='green')
+donation_state_updated_resampled['blood_b'].plot(label='Blood Type B', color='blue')
+donation_state_updated_resampled['blood_o'].plot(label='Blood Type O', color='orange')
+
+plt.title("Yearly Blood Donation Trends")
+plt.xlabel("Year")
+plt.ylabel("Total Blood Donation Count")
+plt.legend()
+plt.show()
+
+
+# Plotting graph for total blood donation based on age category. Assumption of which age category that always come back to donate can be deduce.
+blood_donation_retention_updated['age'] = int(newest_date.year) - blood_donation_retention_updated['birth_date']
+plt.figure(figsize=(16,8))
+idcount = blood_donation_retention_updated['donor_id'].value_counts()
+oldest = blood_donation_retention_updated['birth_date'].min()
+youngest = blood_donation_retention_updated['birth_date'].max()
+age_group_list = newdonors_state_updated.columns[1:11].to_list()
+print(age_group_list)
+print(oldest)
+print(youngest)
+
+bins = [16, 24, 29, 34, 39, 44, 49, 54, 59, 64, 150]
+
+
+blood_donation_retention_updated['age_group'] = pd.cut(blood_donation_retention_updated['age'], bins=bins, labels=age_group_list, right=False)
+
+blood_donation_grouped = blood_donation_retention_updated.groupby(by=['age_group']).count()
+
+
+fig, ax = plt.subplots()
+ax.bar(blood_donation_grouped.index,pd.to_numeric(blood_donation_grouped['age']))
+ax.set_title("Total blood donation retention based on Age Categories ")
+ax.set_xlabel("Age Categories")
+ax.set_ylabel('Total Donation by Millions')
+plt.show()
+
+
+# Average of retention for blood donation per person
+
+donor_grouped = blood_donation_retention_updated.groupby(by = ['donor_id']).count()
+donor_grouped.head(10)
+average_per_person = donor_grouped['visit_date'].mean()
+print(average_per_person)
+
+print("Running Completed")
---
+++ telegrambot_v3.py
@@ -0,0 +1,76 @@
+import telebot
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+from analytics_script import blood_donation_grouped, donation_state_updated_resampled, average_per_person
+from io import BytesIO
+from dotenv import load_dotenv
+from datetime import datetime
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+CHAT_ID = os.getenv('CHAT_ID')
+GRAPH_PIC_FOLDER = "graph_pic"
+bot_ = telebot.TeleBot(TOKEN)
+
+
+@bot_.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot_.reply_to(message, "Send /plot to get today's graph")
+
+
+@bot_.message_handler(commands=['plot'])
+def analytic_graph(message):
+    try:
+        timestamp = datetime.now().strftime("%Y%m%d")
+
+        try:
+            fig, ax = plt.subplots()
+            ax.bar(blood_donation_grouped.index, pd.to_numeric(blood_donation_grouped['age']))
+            ax.set_title("Total blood donation retention based on Age Categories ")
+            ax.set_xlabel("Age Categories")
+            ax.set_ylabel('Total Donation by Millions')
+
+            filename_age_category = f'donation_age_{timestamp}.png'
+            filepath_age_category = os.path.join(GRAPH_PIC_FOLDER, filename_age_category)
+
+            plt.savefig(filepath_age_category, format='png', facecolor='white')
+            plt.clf()
+
+            bot_.send_message(CHAT_ID, "This is the total donation retention based on age category")
+            with open(filepath_age_category, 'rb') as age_photo:
+                bot_.send_photo(CHAT_ID, age_photo)
+
+            donation_state_updated_resampled['blood_a'].plot(label='Blood Type A', color='red')
+            donation_state_updated_resampled['blood_ab'].plot(label='Blood Type AB', color='green')
+            donation_state_updated_resampled['blood_b'].plot(label='Blood Type B', color='blue')
+            donation_state_updated_resampled['blood_o'].plot(label='Blood Type O', color='orange')
+
+            plt.title("Yearly Blood Donation Trends")
+            plt.xlabel("Year")
+            plt.ylabel("Total Blood Donation Count")
+            plt.legend()
+
+            filename_blood_types = f'blood_types_{timestamp}.png'
+            filepath_blood_types = os.path.join(GRAPH_PIC_FOLDER, filename_blood_types)
+
+            plt.savefig(filepath_blood_types, format='png', facecolor='white')
+            plt.clf() 
+
+            bot_.send_message(CHAT_ID, "This is the trend throughout the years till this date for each blood type")
+            with open(filepath_blood_types, 'rb') as blood_type_photo:
+                bot_.send_photo(CHAT_ID, blood_type_photo)
+
+            plt.close()
+
+            bot_.send_message(CHAT_ID, f"An average person donates at least {round(average_per_person)} times throughout their life")
+        except Exception as e:
+            bot_.send_message(CHAT_ID, f"An error occurred: {str(e)}")
+
+    except telebot.apihelper.ApiTelegramException as e:
+        if "Bad Request: chat not found" in str(e):
+            bot_.send_message(CHAT_ID, "Error: Chat not found. Please start a conversation first.")
+
+
+if __name__ == "__main__":
+    bot_.polling()
